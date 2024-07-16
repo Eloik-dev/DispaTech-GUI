e1cad585084c634ac817b90d696c0b955be00fee
Eloik-dev <eloik.rousseau@gmail.com>
2024-07-15 23:06:12 -0400

    Update

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -1,41 +1,124 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include "header/file_manager.h"
 
 using namespace std;
+using json = nlohmann::json;
 namespace fs = filesystem;
 
-FileManager::FileManager(GtkWidget* window) {
+FileManager::FileManager(GtkWidget *window)
+{
     this->window = window;
+    this->currentFileIndex = 0;
     this->initializeFilesDirectory();
-    this->readFiles();
+    this->readFileConfiguration();
+    this->startSlideshow();
 }
 
-void FileManager::initializeFilesDirectory() {
-    try {
+void FileManager::initializeFilesDirectory()
+{
+    try
+    {
         bool created = fs::create_directory(FILES_DIRECTORY);
-        if (created) {
+        if (created)
+        {
             cout << "Files folder was created" << endl;
-        } else {
+        }
+        else
+        {
             cout << "Files folder already exists" << endl;
         }
-    } catch (const exception* ex) {
+    }
+    catch (const exception *ex)
+    {
         cerr << "An error occured during the files directory initialization: " << ex->what() << endl;
     }
 }
 
-void FileManager::readFiles() {
-    try {
-        for (const auto& entry : fs::directory_iterator(FILES_DIRECTORY)) {
-            File file{entry.path().string()};
+void FileManager::readFileConfiguration()
+{
+    try
+    {
+        std::ifstream f(CONFIGURATION_FILE);
+        json configuration = json::parse(f);
+
+        for (json::iterator it = configuration.begin(); it != configuration.end(); ++it)
+        {
+            json file_object = it.value();
+            File *file = new File{
+                file_object["start"].get<int>(),
+                file_object["stop"].get<int>(),
+                file_object["fileName"].get<std::string>()};
+
             this->files.push_back(file);
         }
-    } catch (const exception* ex) {
-            cerr << "An error occured while reading the files directory: " << ex->what() << endl;
+
+        cout << "Read " << this->files.size() << " files to be shown" << endl;
     }
-    
-    for (const File file : this->files) {
-        cout << "Found file: " << file.path << endl;
+    catch (const exception *ex)
+    {
+        cerr << "An error occured while reading the files directory: " << ex->what() << endl;
     }
 }
+
+gboolean FileManager::timeoutCallback(gpointer arguments)
+{
+    cout << "Calling callback" << endl;
+    GtkCallbackData *data = static_cast<GtkCallbackData *>(arguments);
+    File *file = data->file;
+
+    cout << "Showing file " << file->name << endl;
+    string directory = FILES_DIRECTORY;
+    string path = directory + "/" + file->name;
+    cout << path.c_str() << endl;
+    GtkWidget *image = gtk_image_new_from_file(path.c_str());
+    gtk_window_set_child(GTK_WINDOW(data->window), image);
+
+    return G_SOURCE_CONTINUE;
+}
+
+void FileManager::startSlideshow()
+{
+    File *file = this->files[this->currentFileIndex];
+
+    GtkCallbackData *data = new GtkCallbackData{file, this->window};
+
+    g_timeout_add(5000, timeoutCallback, data);
+
+    if (this->currentFileIndex >= this->files.size())
+    {
+        this->currentFileIndex = 0;
+    }
+    else
+    {
+        this->currentFileIndex++;
+    }
+}
+
+/*static gboolean FileManager::showFileRecursive(File *file, int currentFileIndex)
+{
+    try
+    {
+        File *file = this->files[this->currentFileIndex];
+        GtkWidget *image = gtk_image_new_from_file(FILES_DIRECTORY + "/" + file->name);
+        gtk_window_set_child(GTK_WINDOW(window), image);
+    }
+    catch (const exception &ex)
+    {
+        cout << ex.what() << endl;
+    }
+
+    if (this->files.size() > this->currentFileIndex)
+    {
+        currentFileIndex = 0;
+    }
+    else
+    {
+        this->currentFileIndex++;
+    }
+
+    g_timeout_add(3000, this->showFileRecursive, NULL);
+}*/
--- src/header/file_manager.h
+++ src/header/file_manager.h
@@ -2,30 +2,50 @@
 #define FILE_MANAGER_H
 
 #define FILES_DIRECTORY "files"
+#define CONFIGURATION_FILE "configuration.json"
 
 #include <string>
 #include <vector>
 #include <gtk/gtk.h>
 
-struct File {
-    std::string path;
+struct File
+{
+    int start;
+    int stop;
+    std::string name;
+};
+
+struct GtkCallbackData
+{
+    File *file;
+    GtkWidget *window;
 };
 
 class FileManager
 {
 private:
     GtkWidget *window;
-    std::vector<File> files;
+    std::vector<File *> files;
+    std::vector<File *> settings;
+    int currentFileIndex;
 
     /**
-     * Reads all files in the `files` directory
+     * Reads the JSON files configuration
      */
-    void readFiles();
+    void readFileConfiguration();
 
     /**
      * Checks if the `files` directory exists, if not generates it
      */
     void initializeFilesDirectory();
+
+    static gboolean timeoutCallback(gpointer);
+
+    /**
+     * Shows the next file to be shown
+     */
+    void startSlideshow();
+
 public:
     FileManager(GtkWidget *);
 };
